3e457897938bbd3289fd158b642a05b6dc587322
K. Gondow <gondow@cs.titech.ac.jp>
2026-04-27 11:19:03 +0000

    5-3.gdbnb added

--- examples/frame.c
+++ examples/frame.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-void func_B (int n)
+void func_B (int m, int n)
 {
-    int b = 10;
-    printf ("func_B: %d, %d\n", n, b);
+    int b1 = 300, b2 = 400;
+    printf ("func_B: m=%d, n=%d, b1=%d, b2=%d\n", m, n, b1, b2);
 }
 
-void func_A (int n)
+void func_A (int m, int n)
 {
-    int a = 20;
-    func_B (n + 1);
-    printf ("func_A: %d, %d\n", n, a);
+    int a1 = 30, a2 = 40;
+    func_B (100, 200);
+    printf ("func_A: m=%d, n=%d, a1=%d, a2=%d\n", m, n, a1, a2);
 }
 
 int main (void)
 {
-    func_A (100);
+    func_A (10, 20);
 }
